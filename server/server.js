234f1ce5a0859fbbf1a2f66478706942b7f05323
Rasmus <rasmus.peaske@ut.ee>
2023-12-22 15:04:52 +0200

    Task 3 half done (finish styling later)

--- server/server.js
+++ server/server.js
@@ -1,29 +1,44 @@
-
 const express = require('express');
 const pool = require('./database');
-const cors = require('cors')
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const app = express();
-
 
 app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log("Server is listening to port " + port)
+    console.log("Server is listening to port " + port);
 });
 
-
-app.get('/api/courses', async(req, res) => {
+// Get all courses
+app.get('/api/courses', async (req, res) => {
     try {
         console.log("A GET all request has arrived");
-        const course = await pool.query(
-            "SELECT * FROM courses"
-        );
-        res.json(course.rows);
+        const courses = await pool.query("SELECT * FROM courses");
+        res.json(courses.rows);
     } catch (err) {
         console.error(err.message);
+        res.status(500).send('Server Error');
     }
 });
 
+// Get a specific course by ID
+app.get('/api/courses/:id', async (req, res) => {
+    const courseId = req.params.id;
+
+    try {
+        console.log(`A GET request for course ${courseId} has arrived`);
+        const course = await pool.query('SELECT * FROM courses WHERE id = $1', [courseId]);
+
+        if (course.rows.length === 0) {
+            return res.status(404).json({ error: 'Course not found' });
+        }
+
+        res.json(course.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
--- src/views/ACourse.vue
+++ src/views/ACourse.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="ACourse">
-    <h3>Course Details</h3>
-    <!-- Display course details using fetched data based on the route parameter -->
+    <h3>{{ courseData.coursename }} - {{ courseData.coursecode }} - {{ courseData.courseects }} ECTS</h3>
+    
+    <div class="form-container">
+      <div class="form-group">
+        <label>Max Number of Students:</label>
+        <p>{{ courseData.studentsnumbers }}</p>
+      </div>
+
+      <div class="form-group">
+        <label>Number of Groups:</label>
+        <p>{{ courseData.groupsnumbers }}</p>
+      </div>
+
+      <div class="form-group">
+        <label>Course Description:</label>
+        <p>{{ courseData.description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,11 +27,17 @@ export default {
   data() {
     return {
       courseId: null,
+      courseData: {},
     };
   },
   methods: {
     fetchCourseData() {
-      
+      fetch(`http://localhost:3000/api/courses/${this.courseId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.courseData = data;
+        })
+        .catch((err) => console.log(err.message));
     },
   },
   mounted() {
@@ -27,5 +49,17 @@ export default {
 </script>
 
 <style scoped>
+/* Add your styles as needed */
+.form-container {
+  max-width: 600px; /* Adjust the maximum width as needed */
+  margin: 20px auto; /* Center the form container */
+}
 
+.form-group {
+  margin-bottom: 15px;
+}
+
+label {
+  font-weight: bold;
+}
 </style>
