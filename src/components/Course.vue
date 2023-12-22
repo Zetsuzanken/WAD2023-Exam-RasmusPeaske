e447281768fb0d9c11252003f2e4a21b83e74996
Rasmus <rasmus.peaske@ut.ee>
2023-12-22 14:47:44 +0200

    Task 1 half done (finish styling later)

---
+++ src/components/Course.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="courses-container">
+    <div class="course-details">
+      <div class="detail-row">
+        <strong>Course name:</strong>
+        <p>{{ course.coursename }}</p>
+      </div>
+      <div class="detail-row">
+        <strong>Course code:</strong>
+        <p>{{ course.coursecode }}</p>
+      </div>
+      <div class="detail-row">
+        <strong>ECTS:</strong>
+        <p>{{ course.courseects }}</p>
+      </div>
+      <div class="detail-row">
+        <strong>Students:</strong>
+        <p>{{ course.studentsnumbers }}</p>
+      </div>
+      <div class="detail-row">
+        <strong>Groups:</strong>
+        <p>{{ course.groupsnumbers }}</p>
+      </div>
+      <div class="detail-row">
+        <strong>Description:</strong>
+        <p>{{ course.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: Object,
+  },
+};
+</script>
+
+<style scoped>
+.courses-container {
+  display: flex;
+}
+
+.course-details {
+  display: flex;
+  flex-direction: row;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-right: 15px;
+}
+
+.detail-row {
+  margin-right: 20px;
+}
+
+.detail-row strong {
+  display: block;
+  margin-bottom: 5px;
+}
+</style>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,16 +1,18 @@
 <template>
-<h3>Courses</h3>
-
-
+  <div>
+    <h3>Courses</h3>
+    <course v-for="course in courses" :key="course.id" :course="course" />
+  </div>
 </template>
 
 <script>
+import Course from "@/components/Course.vue";
 
 export default {
   name: "Courses",
-    components: {
-
-    },
+  components: {
+    Course,
+  },
   data() {
     return {
       courses: [],
@@ -22,19 +24,19 @@ export default {
         .then((response) => response.json())
         .then((data) => (this.courses = data))
         .catch((err) => console.log(err.message));
-  },
+    },
   },
   mounted() {
     this.fetchRecords();
     console.log("mounted");
-  } 
+  },
 };
 </script>
 
 <style scoped>
-
 h3 {
   text-align: center;
   color: rgb(8, 110, 110);
 }
 </style>
+s
